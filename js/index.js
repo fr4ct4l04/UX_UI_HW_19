33e67ebb654e3e223d1d8b30cdf786066628dd7f
fr4ct4l <gabrielle.davis999@gmail.com>
2023-10-18 13:55:09 -0500

    Update index.js
    Trying to write a button color change on hover

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log("Your index.js file is loaded correctly!");
 
-$(".send-button").on("click", function (){
-    $('.cssChange').css("background-color","#fbdea0");
+$(".send-button").on("hover", function (){
+    $('.cssChange').css("background-color","red");
 });
