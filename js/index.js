4b23b5e1287b67122cfa52159c9dd9cf7cafcaf3
fr4ct4l <gabrielle.davis999@gmail.com>
2023-10-18 18:59:36 -0500

    Adding jQuery to html
    added jquery to html for interaction

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log("Your index.js file is loaded correctly!");
 
-$(".send-button").on("hover", function (){
-    $('.cssChange').css("background-color","red");
+$(".send-button").hover(function (){
+    $(this).css("background-color","#FBDAE0");
 });
