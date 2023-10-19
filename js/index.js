dc2799029388aa9141c079c43ccd928870bd13f6
fr4ct4l <gabrielle.davis999@gmail.com>
2023-10-18 21:46:32 -0500

    Updates
    Changed send-button class and added a new jquery (mouseover) to replace hover

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log("Your index.js file is loaded correctly!");
 
-$(".send-button").hover(function (){
+$(".send-button").on("mouseover", function (){
     $(this).css("background-color","#FBDAE0");
 });
