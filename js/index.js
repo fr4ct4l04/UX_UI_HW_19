bd662750f9f9f09e138ae632603f95cd76a1447d
fr4ct4l <gabrielle.davis999@gmail.com>
2023-10-18 21:48:18 -0500

    Mouseleave
    Added mouseleavee jquery to change button back to normal state

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@ console.log("Your index.js file is loaded correctly!");
 $(".send-button").on("mouseover", function (){
     $(this).css("background-color","#FBDAE0");
 });
+
+$(".send-button").on("mouseleave", function (){
+    $(this).css("background-color","#660018");
+});
